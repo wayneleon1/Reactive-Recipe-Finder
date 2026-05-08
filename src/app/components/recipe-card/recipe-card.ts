c9f6b27acc5da3f5b954aa0859d342bbd72bab10
wayneleon1 <rurangwaleon10@gmail.com>
2026-05-08 19:35:46 +0200

    Add RecipeCardComponent with hover effects

---
+++ src/app/components/recipe-card/recipe-card.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Recipe } from '../../models/recipe.model';
+import { Button } from '../button/button';
+
+@Component({
+  selector: 'app-recipe-card',
+  standalone: true,
+  imports: [CommonModule, Button],
+  templateUrl: './recipe-card.html',
+  styleUrl: './recipe-card.css',
+})
+export class RecipeCardComponent {
+  readonly recipe = input.required<Recipe>();
+
+  readonly viewRecipe = output<Recipe>();
+
+  onViewRecipe(): void {
+    this.viewRecipe.emit(this.recipe());
+  }
+
+  get totalMinutes(): number {
+    return this.recipe().prepMinutes + this.recipe().cookMinutes;
+  }
+}
